a1a61667f00b9b644a948d296981a0cb8c8b0c5a
vladvaldtitov <vlad.vlad.titov@gmail.com>
2015-12-28 21:32:52 -0500

    remove account

--- Pull.php
+++ Pull.php
@@ -1,6 +1,6 @@
 <?
-$id = isset($_SESSION['directories_userid'])?$_SESSION['directories_userid']:null;
-if(isset($_SESSION['directories_userid']) && $_SESSION['directories_userid']){
+$id = isset($_SESSION['directories_userid'])?$_SESSION['directories_userid']:0;
+if($id){
 
     echo shell_exec('git pull 2>&1');
 }else echo 'Hello World '.$id;
