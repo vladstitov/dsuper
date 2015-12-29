f581ba4b395859ddacde274c55f045385c23ba79
vladvaldtitov <vlad.vlad.titov@gmail.com>
2015-12-28 21:04:39 -0500

    log install

--- Pull.php
+++ Pull.php
@@ -1,3 +1,6 @@
 <?
-echo shell_exec('git pull 2>&1');
+if(isset($_SESSION['directories_userid']) && $_SESSION['directories_userid']){
+
+    echo shell_exec('git pull 2>&1');
+}else echo 'Hello World';
 ?>
